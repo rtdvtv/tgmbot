f46daf1e4c3a61ba4ecb8c9598905a7b99d9fe34
rtdvtv <rtdvtv@gmail.com>
2025-02-22 22:37:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-BOT_TOKEN = '7371473685:AAGCDO0VpFZ_Qn00dpfEpAc0ZShw4skKbgI'
-MISTRAL_API_KEY = "mCooJW4vlhaTb6HxIHfZPt5jqxBWvHse"
+BOT_TOKEN = 'YOUR TOKEN'
+MISTRAL_API_KEY = "your key"
 # ID чата для отправки сообщений
-CHAT_ID = "520410415"  # Например, ID администратора или группы
-#520410415
-#6116969753
-#7268243700
+CHAT_ID = "your chat_id"  # Например, ID администратора или группы
 
 # Ссылка на поток для просмотра онлайн в браузере
 URL_ONLINE_TV = "http://cdn-br2.live-tv.cloud:8081/tgtv/1080i/playlist.m3u8"
