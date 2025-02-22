0bc32577cad83f4d50d42e8d6e19a0b6cfc130da
rtdvtv <rtdvtv@gmail.com>
2025-02-22 22:38:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ MISTRAL_API_KEY = "your key"
 CHAT_ID = "your chat_id"  # Например, ID администратора или группы
 
 # Ссылка на поток для просмотра онлайн в браузере
-URL_ONLINE_TV = "http://cdn-br2.live-tv.cloud:8081/tgtv/1080i/playlist.m3u8"
+URL_ONLINE_TV = "your URL m3u8"
 # URL M3U8:
-URL_M3U8 = "http://cdn-br2.live-tv.cloud:8081/tgtv/1080i/playlist.m3u8"
+URL_M3U8 = "your URL m3u8"
 
 
 # ГЛОБАЛЬНАЯ ПЕРЕМЕННАЯ - URL для потока (URL по умолчанию)
